d49ed3d899737566fa08e11949df6b82af6fce73
Jack Ground <jaground@gmail.com>
2019-08-26 15:11:51 -0500

    reconneted API

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,5 +1,5 @@
 // Restaurant Controller
-let testData = require('../testData');
+// let testData = require('../testData');
 
 const Restaurant = require('../models/restaurant');
 const zomato = require('zomato');
@@ -12,25 +12,27 @@ module.exports = {
 }
 
 
-function index (req, res) {
-    res.render('restaurants/index', {
-        title: 'restaurants', 
-        testData});
-}
+// function index (req, res) {
+//     res.render('restaurants/index', {
+//         title: 'restaurants', 
+//         testData});
+// }
 
 
 /// commented out API call to not stack up calls during CSS design
-// function index (req, res) {
-//    let lat = req.params.id.slice(0,10)
-//    let lon = req.params.id.slice(19)
-//     client.search({
-//         q: "taco", //Search Keyword
-//         lat, //latitude
-//         lon, //longitude
-//         count: "2", // number of maximum result to display
-//         radius: "500", //radius around (lat,lon); to define search area, defined in meters(M)
-//     }, function (err, results) {
-//         if (err) console.log(err);
-//         else res.render('restaurants/index', {testData: JSON.parse(results)})
-//     });
-// }
+function index (req, res) {
+   let lat = req.params.id.slice(0,10)
+   let lon = req.params.id.slice(19)
+    client.search({
+        q: "taco", //Search Keyword
+        lat, //latitude
+        lon, //longitude
+        count: "2", // number of maximum result to display
+        radius: "500", //radius around (lat,lon); to define search area, defined in meters(M)
+    }, function (err, results) {
+        if (err) console.log(err);
+        else res.render('restaurants/index', {
+            title: 'restaurants',
+            testData: JSON.parse(results)})
+    });
+}
