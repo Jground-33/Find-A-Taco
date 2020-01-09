acc3dc5a172336521d225074bbc7e47b73e2fa96
Jack Ground <jaground@gmail.com>
2020-01-09 08:03:07 -0600

    updates npm package and new API client ID

--- controllers/users.js
+++ controllers/users.js
@@ -10,34 +10,40 @@ module.exports = {
 
 async function index(req, res) {
     try {
-    let user = await User.findOne({googleId: req.user.googleId}).populate('favorites');
-    res.render('users/index', {
-        title: 'welcome user',
-        user,
-        restaurants: user.favorites,
-    });
-    } catch(e) {console.error(e);}
+        let user = await User.findOne({googleId: req.user.googleId}).populate('favorites');
+        res.render('users/index', {
+            title: 'welcome user',
+            user,
+            restaurants: user.favorites,
+        });
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 async function addFavorite(req, res) {
     try {
-    let restaurant = await Restaurant.findOne({api_id: req.params.id});
-    let user = await User.findOne({googleId: req.user.googleId});
-    user.favorites.push(restaurant.id);
-    user = await user.save();
+        let restaurant = await Restaurant.findOne({api_id: req.params.id});
+        let user = await User.findOne({googleId: req.user.googleId});
+        user.favorites.push(restaurant.id);
+        user = await user.save();
         res.redirect(`/restaurants/${req.params.id}/show`);
-    } catch(e) {console.error(e)}
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 async function removeFavorite(req, res) {
     try {
-    let user = await User.findOne({googleId: req.user.googleId}).populate('favorites');
-    let filteredFavs = user.favorites.filter(obj => {
-        return obj.api_id !== req.params.id;
-     });
-     user.favorites = filteredFavs;
-     user = await user.save();
-    res.redirect(`/restaurants/${req.params.id}/show`);
-    } catch(e) {console.error(e)}
+        let user = await User.findOne({googleId: req.user.googleId}).populate('favorites');
+        let filteredFavs = user.favorites.filter(obj => {
+            return obj.api_id !== req.params.id;
+        });
+        user.favorites = filteredFavs;
+        user = await user.save();
+        // res.redirect(`/restaurants/${req.params.id}/show`);
+        res.redirect('/users')
+    } catch (e) {
+        console.error(e)
+    }
 }
-
